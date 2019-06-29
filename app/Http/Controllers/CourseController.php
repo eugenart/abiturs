77fd7053b24c65b89abb3ef99bc0e53291a14f47
alexander <alerogov@gmail.com>
2019-06-29 14:26:33 +0300

    29.06.19 Update

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -101,33 +101,78 @@ class CourseController extends Controller
                 'name' => $request->name,
                 'score' => $request->score,
             ]);
+            $course->intramural = $course->studyForms->where('name', 'Очная')->first();
+            $course->partTime = $course->studyForms->where('name', 'Очно-заочная')->first();
+            $course->correspondence = $course->studyForms->where('name', 'Заочная')->first();
             if ($request->intramural) {
-                StudyForms::create([
-                    'name' => $request->intramural['name'],
-                    'budget' => (isset($request->intramural['budget']) ? $request->intramural['budget'] : '-'),
-                    'price' => (isset($request->intramural['price']) ? $request->intramural['price'] : '-'),
-                    'year' => (isset($request->intramural['year']) ? $request->intramural['year'] : '-'),
-                    'course_id' => $course->id,
-                ]);
+                if ($course->intramural) {
+                    $course->intramural->update([
+                        'name' => $request->intramural['name'],
+                        'budget' => (isset($request->intramural['budget']) ? $request->intramural['budget'] : '-'),
+                        'price' => (isset($request->intramural['price']) ? $request->intramural['price'] : '-'),
+                        'year' => (isset($request->intramural['year']) ? $request->intramural['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                } else {
+                    StudyForms::create([
+                        'name' => $request->intramural['name'],
+                        'budget' => (isset($request->intramural['budget']) ? $request->intramural['budget'] : '-'),
+                        'price' => (isset($request->intramural['price']) ? $request->intramural['price'] : '-'),
+                        'year' => (isset($request->intramural['year']) ? $request->intramural['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                }
             }
+            if (!isset($request->intramural) && $course->intramural !== null) {
+                $course->intramural->delete();
+            }
+
             if ($request->partTime) {
-                StudyForms::create([
-                    'name' => $request->partTime['name'],
-                    'budget' => (isset($request->partTime['budget']) ? $request->partTime['budget'] : '-'),
-                    'price' => (isset($request->partTime['price']) ? $request->partTime['price'] : '-'),
-                    'year' => (isset($request->partTime['year']) ? $request->partTime['year'] : '-'),
-                    'course_id' => $course->id,
-                ]);
+                if ($course->partTime) {
+                    $course->partTime->update([
+                        'name' => $request->partTime['name'],
+                        'budget' => (isset($request->partTime['budget']) ? $request->partTime['budget'] : '-'),
+                        'price' => (isset($request->partTime['price']) ? $request->partTime['price'] : '-'),
+                        'year' => (isset($request->partTime['year']) ? $request->partTime['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                } else {
+                    StudyForms::create([
+                        'name' => $request->partTime['name'],
+                        'budget' => (isset($request->partTime['budget']) ? $request->partTime['budget'] : '-'),
+                        'price' => (isset($request->partTime['price']) ? $request->partTime['price'] : '-'),
+                        'year' => (isset($request->partTime['year']) ? $request->partTime['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                }
             }
+            if (!isset($request->partTime) && $course->partTime !== null) {
+                $course->partTime->delete();
+            }
+
             if ($request->correspondence) {
-                StudyForms::create([
-                    'name' => $request->correspondence['name'],
-                    'budget' => (isset($request->correspondence['budget']) ? $request->correspondence['budget'] : '-'),
-                    'price' => (isset($request->correspondence['price']) ? $request->correspondence['price'] : '-'),
-                    'year' => (isset($request->correspondence['year']) ? $request->correspondence['year'] : '-'),
-                    'course_id' => $course->id,
-                ]);
+                if ($course->correspondence) {
+                    $course->correspondence->update([
+                        'name' => $request->correspondence['name'],
+                        'budget' => (isset($request->correspondence['budget']) ? $request->correspondence['budget'] : '-'),
+                        'price' => (isset($request->correspondence['price']) ? $request->correspondence['price'] : '-'),
+                        'year' => (isset($request->correspondence['year']) ? $request->correspondence['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                } else {
+                    StudyForms::create([
+                        'name' => $request->correspondence['name'],
+                        'budget' => (isset($request->correspondence['budget']) ? $request->correspondence['budget'] : '-'),
+                        'price' => (isset($request->correspondence['price']) ? $request->correspondence['price'] : '-'),
+                        'year' => (isset($request->correspondence['year']) ? $request->correspondence['year'] : '-'),
+                        'course_id' => $course->id,
+                    ]);
+                }
             }
+            if (!isset($request->correspondence) && $course->correspondence !== null) {
+                $course->correspondence->delete();
+            }
+
             return response()->json($course, 200);
         }
         return response()->json(['message' => 'Oops'], 404);
