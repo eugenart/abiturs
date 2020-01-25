a6d7fbf6dd616f591223328fef3985da5e885aa4
ElionBlack <elionorablack@gmail.com>
2020-01-25 18:06:25 +0300

    stat2

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -83,6 +83,20 @@ class StatisticController extends Controller
     public function search(Request $request)
     {
         //dd($request->all());
+//        $fio = $request->fio;
+//        $students = Student::where('fio', 'LIKE', '%' . $fio . '%')->get(); //студенты все по имени
+//        $id_stud_arr = array();
+//
+//
+//        foreach ($students as $student) {
+//            $id_stud_arr[] = $student->id;
+//        }
+//
+//        //выбрать статистику по этим студентам
+//        $stat = Statistic::whereIn('id_student', $id_stud_arr)->get();
+//        //return $stat;
+//        return view('pages.stat', ['statistics' => $stat]);
+
         $fio = $request->fio;
         $students = Student::where('fio', 'LIKE', '%' . $fio . '%')->get(); //студенты все по имени
         $id_stud_arr = array();
@@ -92,10 +106,57 @@ class StatisticController extends Controller
             $id_stud_arr[] = $student->id;
         }
 
-        //выбрать статистику по этим студентам
-        $stat = Statistic::whereIn('id_student', $id_stud_arr)->get();
-        //return $stat;
-        return view('pages.stat', ['statistics' => $stat]);
+
+        $studyForms = StudyForm::all();
+
+        foreach ($studyForms as $studyForm) {
+            $categories = Category::all();
+            foreach ($categories as $category) {
+                $admissionBases = AdmissionBasis::all();
+                foreach ($admissionBases as $admissionBasis) {
+                    $preparationLevels = PreparationLevel::all();
+                    foreach ($preparationLevels as $preparationLevel) {
+                        $faculties = Faculty::all();
+                        foreach ($faculties as $faculty) {
+                            $specialities_id = DB::table('statistics')
+                                ->where('id_faculty', '=', $faculty->id)
+                                ->select('statistics.id_speciality')
+                                ->distinct()
+                                ->get();
+                            $id_spec_arr = array();
+                            foreach ($specialities_id as $item) {
+                                $id_spec_arr[] = $item->id_speciality;
+                            }
+                            $specialities = Speciality::whereIn('id', $id_spec_arr)->get();
+
+                            foreach ($specialities as $speciality) {
+                                $temp = Statistic::where('id_studyForm', '=', $studyForm->id)
+                                    ->where('id_preparationLevel', '=', $preparationLevel->id)
+                                    ->where('id_admissionBasis', '=', $admissionBasis->id)
+                                    ->where('id_category', '=', $category->id)
+                                    ->where('id_faculty', '=', $faculty->id)
+                                    ->where('id_speciality', '=', $speciality->id)
+                                    ->whereIn('id_student', $id_stud_arr)
+                                    ->get();
+                                // $temp->count() ? $tempspec[] = $temp : null;
+                                $speciality->abiturs = $temp;
+                            }
+                            //count($tempspec) ? $tempfacs[] = $tempspec : null;
+                            $faculty->specialities = $specialities;
+                        }
+                        //count($tempfacs) ? $temppreps[] = $tempfacs : null;
+                        $preparationLevel->faculties = $faculties;
+                    }
+                    //count($temppreps) ? $tempstats[] = $temppreps : null;
+                    $admissionBasis->preparationLevels = $preparationLevels;
+                }
+                // $tempForms[] = $tempstats;
+                $category->admissionBases = $admissionBases;
+            }
+            // $studyForm->stat = $tempForms;
+            $studyForm->stat = $categories;
+        }
+        return view('pages.stat',compact('studyForms'));
     }
 
 }
