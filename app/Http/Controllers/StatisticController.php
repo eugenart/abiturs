2b9d6592b26646bc82874f54503a513a76a63507
ElionBlack <elionorablack@gmail.com>
2020-01-25 16:43:32 +0300

    three ugly tables 2

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -2,39 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\AdmissionBasis;
+use App\Category;
 use App\Faculty;
+use App\PreparationLevel;
 use App\Speciality;
 use App\Statistic;
+use App\StatisticsExtra;
 use App\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StatisticController extends Controller
 {
     public function index(Request $request)
     {
-        //$statistics = Statistic::all();
+
+        $categories = Category::all();
+        $admissionBases = AdmissionBasis::all();
+        $preparationLevels = PreparationLevel::all();
         $faculties = Faculty::all();
-        foreach ($faculties as $fac) {
-            $statFac = Statistic::where('id_faculty', '=', $fac->id)->get();
-            //$fac->statFac = Statistic::where('id_faculty', '=', $fac->id)->get();
-            $specialities = [];
-            foreach ($statFac as $item) {
-                $specialities[] = $item->id_speciality;
-            }
-            $fac->specialities = array_values(array_unique($specialities));
-            $statSpec = [];
-            $preparationLevel = [];
-            foreach ($fac->specialities as $spec) {
-                $statSpec[] = Statistic::where('id_speciality', '=', $spec)->get();
-            }
-            foreach ($statSpec as $stsp) {
-                $preparationLevel[] = $stsp->preparationLevel;
-            }
-            $fac->statSpec = $statSpec;
+//        $specialities = Speciality::all();
 
+        foreach ($categories as $category) {
+            $tempstats = array();
+            foreach ($admissionBases as $admissionBasis) {
+                $temppreps = array();
+                foreach ($preparationLevels as $preparationLevel) {
+                    $tempfacs = array();
+                    foreach ($faculties as $faculty) {
+                        $tempspec = array();
+                        $specialities = DB::table('statistics_extras')
+                            ->where('id_faculty', '=', $faculty->id)
+                            ->select('statistics_extras.id_speciality')
+                            ->distinct()
+                            ->get();
+                        foreach ($specialities as $speciality){
+                            $temp = StatisticsExtra::where('id_preparationLevel', '=', $preparationLevel->id)
+                                ->where('id_admissionBasis', '=', $admissionBasis->id)
+                                ->where('id_category', '=', $category->id)
+                                ->where('id_faculty', '=', $faculty->id)
+                                ->where('id_speciality', '=', $speciality->id_speciality)
+                                ->get();
+                            $temp->count() ? $tempspec[] = $temp : null;
+                        }
+                        count($tempspec) ? $tempfacs[] = $tempspec : null;
+                    }
+                    count($tempfacs) ? $temppreps[] = $tempfacs : null;
+                }
+                count($temppreps) ?  $tempstats[] = $temppreps : null;
+            }
+            $category->statWithBasis = $tempstats;
         }
+        $category->stat = [];
+        return $categories;
 
-        return json_encode($faculties, JSON_UNESCAPED_UNICODE);
+
+        //$statistics = Statistic::all();
+//        $faculties = Faculty::all();
+//        foreach ($faculties as $fac) {
+//            $statFac = Statistic::where('id_faculty', '=', $fac->id)->get();
+//            //$fac->statFac = Statistic::where('id_faculty', '=', $fac->id)->get();
+//            $specialities = [];
+//            foreach ($statFac as $item) {
+//                $specialities[] = $item->id_speciality;
+//            }
+//            $fac->specialities = array_values(array_unique($specialities));
+//            $statSpec = [];
+//            $preparationLevel = [];
+//            foreach ($fac->specialities as $spec) {
+//                $statSpec[] = Statistic::where('id_speciality', '=', $spec)->get();
+//            }
+//            foreach ($statSpec as $stsp) {
+//                $preparationLevel[] = $stsp->preparationLevel;
+//            }
+//            $fac->statSpec = $statSpec;
+//
+//        }
+//
+//        return json_encode($faculties, JSON_UNESCAPED_UNICODE);
 
 //        return view('pages.stat', compact('statistics', 'faculties', 'specialities'));
 //        return view('pages.stat', [ 'statistics' => $statistics
