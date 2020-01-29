7e4ce7dbb85a32ad9acea6ea092d0ba87161e630
evgenijartaskin <eugen.art@mail.ru>
2020-01-29 12:55:54 +0300

    meta

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -2,6 +2,21 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
+
+    <meta property="og:title" content="Приёмная кампания МГУ им. Н.П. Огарева 2020."/>
+    <meta property="og:description" content="Приёмная кампания МГУ им. Н.П. Огарева 2020. Статистика рпиёма, подбор направления."/>
+    <meta property="og:image" content="https://abitur.mrsu.ru/storage/preview/20200128_163227_image.jpeg"/>
+    <meta property="og:type" content="profile"/>
+    <meta property="og:url" content= "https://abitur.mrsu.ru/" />
+
+    <meta name="keywords"
+          content="Приемная кампания, МГУ Огарева, МГУ им. Н.П. Огарева, приемная комиссия, приемная комиссия 2020"/>
+    <meta name="subject" content="Приемная кампания МГУ им. Н.П. Огарева 2020">
+    <meta name="author" content="Арташкин Евгений, Элина Кирдяшкина">
+    <meta name="robots" content="index,follow" />
+    <meta name="description" content="Приемная кампания МГУ им. Н.П. Огарева 2020"/>
+    <meta name="url" content="https://abitur.mrsu.ru/">
+    <meta name="identifier-URL" content="https://abitur.mrsu.ru/">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
