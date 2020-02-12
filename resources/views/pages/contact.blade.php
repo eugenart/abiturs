877ea988bb1b6173c183e62280a9ee6ffc9a4c78
evgenijartaskin <eugen.art@mail.ru>
2020-02-12 09:08:54 +0300

    mrsu style 12

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,7 +7,7 @@
                 <h1 class="text-center main-color">Остались вопросы? Напишите нам</h1>
                 <h5 class="text-center">Воспользуйтесь формой обратной связи, чтобы задать интересующие Вас
                     вопросы:</h5>
-                <form action="" class="contact-us d-flex align-items-center justify-content-center flex-column">
+                <form action="" class="contact-us d-flex align-items-center justify-content-center flex-column p-0">
                     <input name="fio" type="text" placeholder="ФИО *" required>
                     <input name="email" type="email" placeholder="Email *" required>
                     <input id="phone" name="phone" type="tel" placeholder="Номер телефона *" required>
