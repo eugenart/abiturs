779872eb2e94b39cc76b6b4fe41beffaef83bc85
El Black <elionorablack@gmail.com>
2020-05-23 18:15:47 +0300

    ftp 8.1

--- app/Http/Controllers/DownloadFileController.php
+++ app/Http/Controllers/DownloadFileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ParserXlsTrait;
+use App\Traits\ParserJsonTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\ParserJsonController as ParserJsonController;
@@ -11,6 +12,7 @@ use App\Http\Controllers\ParserController;
 class DownloadFileController extends Controller
 {
     use ParserXlsTrait;
+    use ParserJsonTrait;
 
     public function index(Request $request)
     {
@@ -40,7 +42,7 @@ class DownloadFileController extends Controller
                     $result = $this->parseSubjects();
                     return $result;
                 }
-            } elseif ($param == "admission_bases"){
+            } elseif ($param == "admission_bases") {
                 $res = $this->download($directory, "admission_bases.xls");
                 if ($res === 0) {
                     $result = $this->parseAdmissionBases();
@@ -50,26 +52,88 @@ class DownloadFileController extends Controller
         } elseif ($param == "past_contests") {
             $directory = "pastContests";
             $res = $this->download($directory, "past_contests.json");
-//            if ($res === 0) {
-//                $result = $this->parseAdmissionBases();
-//                return $result;
-//            }
+            if ($res === 0) {
+                $result = $this->parsePastContests();
+                return $result;
+            }
         } elseif
-        ($param == "stat_bach" || $param == "stat_master"
-            || $param == "stat_asp" || $param == "stat_spo") {
+        ($param == "stat_bach" || $param == "stat_master" || $param == "stat_asp" || $param == "stat_spo" || $param == "stat_bach_catalogs") {
             $directory = "statistics";
+            if ($param == "stat_bach_catalogs") {
+                $res = $this->download($directory, "stat_bach.json");
+                if ($res === 0) {
+                    $result = $this->parseCatalogs();
+                    return $result;
+                }
+            } elseif ($param == "stat_bach") {
+                $res = $this->download($directory, "stat_bach.json");
+                if ($res === 0) {
+                    $result = $this->parseStatBachAll();
+                    return $result;
+                }
+            } elseif ($param == "stat_master") {
+                $res = $this->download($directory, "stat_master.json");
+                if ($res === 0) {
+                    $result = $this->parseStatMasterAll();
+                    return $result;
+                }
+            } elseif ($param == "stat_asp") {
+                $res = $this->download($directory, "stat_asp.json");
+                if ($res === 0) {
+                    $result = $this->parseStatAspAll();
+                    return $result;
+                }
+            } elseif ($param == "stat_spo") {
+                $res = $this->download($directory, "stat_spo.json");
+                if ($res === 0) {
+                    $result = $this->parseStatSpoAll();
+                    return $result;
+                }
+            }
+        } elseif ($param == "plans_bach" || $param == "plans_master" || $param == "plans_asp" || $param == "plans_spo") {
+            if ($param == "plans_bach") {
+                $directory = "plans/plans_saransk";
+                $res = $this->download($directory, "plans_sar_bach.json");
+                $directory = "plans/plans_rim";
+                $res1 = $this->download($directory, "plans_rim_bach.json");
+                if ($res === 0 && $res1 === 0) {
+                    $result = $this->parsePlansBach();
+                    return $result;
+                }
+            }elseif ($param == "plans_master"){
+                $directory = "plans/plans_saransk";
+                $res = $this->download($directory, "plans_sar_master.json");
+                $directory = "plans/plans_rim";
+                $res1 = $this->download($directory, "plans_rim_master.json");
+                if ($res === 0 && $res1 === 0) {
+                    $result = $this->parsePlansMaster();
+                    return $result;
+                }
+            }elseif ($param == "plans_asp"){
+                $directory = "plans/plans_saransk";
+                $res = $this->download($directory, "plans_sar_asp.json");
+                if ($res === 0) {
+                    $result = $this->parsePlansAspMain();
+                    return $result;
+                }
+            }elseif ($param == "plans_spo"){
+                $directory = "plans/plans_saransk";
+                $res = $this->download($directory, "plans_sar_spo.json");
+                $directory = "plans/plans_rim";
+                $res1 = $this->download($directory, "plans_rim_spo.json");
+                $directory = "plans/plans_kov";
+                $res2 = $this->download($directory, "plans_kov_spo.json");
+                if ($res === 0 && $res1 === 0 && $res2 === 0) {
+                    $result = $this->parsePlansSpo();
+                    return $result;
+                }
+            }
         }
 
     }
 
-    public
-    function show(Request $request)
-    {
-        return view('pages.files');
-    }
 
-    public
-    function download($directory, $file_name)
+    public function download($directory, $file_name)
     {
         $host = '194.54.64.15';
         $port = 22;
@@ -114,8 +178,8 @@ class DownloadFileController extends Controller
             if (file_exists($remote_file_path) && file_exists($local_file_path)) {
                 if (filesize($remote_file_path) == filesize($local_file_path)
                     && md5_file($remote_file_path) == md5_file($local_file_path)) {
-                    echo "Файлы ". $file ." совпадают.\n";
-                    return 1;
+                    echo "Файлы " . $file . " совпадают.\n";
+                    return 0;
                 } else {
                     if (!$remote = @fopen("ssh2.sftp://{$stream}/{$remoteDir}/{$file}", 'r')) {
                         die("Невозможно открыть файл на удаленном сервере: $file\n");
@@ -139,6 +203,8 @@ class DownloadFileController extends Controller
                             && md5_file($remote_file_path) == md5_file($local_file_path)) {
 //                            echo "Файл ". $file ." успешно загружен.\n";
                             return 0;
+                        } else {
+                            return 1;
                         }
                     }
                     fclose($local);
@@ -167,6 +233,8 @@ class DownloadFileController extends Controller
                         && md5_file($remote_file_path) == md5_file($local_file_path)) {
 //                        echo json_encode("Файл ". $file ." успешно загружен.\n");
                         return 0;
+                    } else {
+                        return 1;
                     }
                 }
                 fclose($local);
@@ -178,198 +246,11 @@ class DownloadFileController extends Controller
         }
     }
 
-
-    public
-    function iddex(Request $request)
+    public function show(Request $request)
     {
-
-
-//        echo $request->file_name;
-        $file_names = $request->file_name;
-        $file_name_arr = explode(" ", $file_names);
-//        var_dump($file_name_arr);
-
-        $host = '194.54.64.15';
-        $port = 22;
-        $username = 'icmrsu';
-        $password = 'KUGyjk76$$q@';
-
-        if ($file_name_arr[0] == "disciplines.xls" || $file_name_arr[0] == "specializations.xls"
-            || $file_name_arr[0] == "specialities.xls" || $file_name_arr[0] == "admission_bases.xls"
-            || $file_name_arr[0] == "faculties.xls") {
-            $directory = "catalogs";
-        } elseif ($file_name_arr[0] == "past_contests.json") {
-            $directory = "pastContests";
-        } elseif ($file_name_arr[0] == "stat_asp.json" || $file_name_arr[0] == "stat_bach.json"
-            || $file_name_arr[0] == "stat_master.json" || $file_name_arr[0] == "stat_spo.json") {
-            $directory = "statistics";
-        } elseif ($file_name_arr[0] == "plans_kov_spo.json") {
-            $directory = "plans/plans_kov";
-        } elseif ($file_name_arr[0] == "plans_rim_bach.json" || $file_name_arr[0] == "plans_rim_master.json" || $file_name_arr[0] == "plans_rim_spo.json") {
-            $directory = "plans/plans_rim";
-        } elseif ($file_name_arr[0] == "plans_sar_master.json" || $file_name_arr[0] == "plans_sar_bach.json"
-            || $file_name_arr[0] == "plans_sar_spo.json" || $file_name_arr[0] == "plans_sar_asp.json"
-            || $file_name_arr[0] == "stat_bach_catalogs") {
-            $directory = "plans/plans_saransk";
-        }
-
-
-        $remoteDir = '/home/icmrsu/' . $directory;
-//        $localDir = '/var/www/html/abiturs/storage/app/public/files/'. $directory;
-        $localDir = 'D:\OSPanel\domains\abiturs\storage\app\public\files\\' . $directory;
-        //echo $localDir;
-
-        if (!function_exists("ssh2_connect"))
-            die('Function ssh2_connect not found, you cannot use ssh2 here');
-
-        if (!$connection = ssh2_connect($host, $port))
-            die('Unable to connect');
-
-        if (!ssh2_auth_password($connection, $username, $password))
-            die('Unable to authenticate.');
-
-        if (!$stream = ssh2_sftp($connection))
-            die('Unable to create a stream.');
-
-
-        if (!$dir = scandir("ssh2.sftp://{$stream}{$remoteDir}"))
-            die('Could not open the directory');
-
-        $files = array();
-        foreach ($dir as $file) {
-            if ($file == "." || $file == "..")
-                continue;
-            $files[] = $file;
-        }
-
-//        var_dump($files);
-//        echo "</br>";
-
-
-        foreach ($file_name_arr as $file_name) {
-            $temp = "";
-            if ($file_name == "stat_bach_catalogs") {
-                $file_name = "stat_bach.json";
-                $temp = "stat_bach_catalogs";
-            }
-            $key = array_search($file_name, $files);
-            if (!($key === false)) {
-                $file = $files[$key];
-
-                // echo "Copying file: $file\n";
-
-                $remote_file_path = "ssh2.sftp://{$stream}/{$remoteDir}/{$file}";
-                $local_file_path = $localDir . '/' . $file;
-
-                if (file_exists($remote_file_path) && file_exists($local_file_path)) {
-                    if (filesize($remote_file_path) == filesize($local_file_path)
-                        && md5_file($remote_file_path) == md5_file($local_file_path)) {
-                        echo "Файлы совпадают";
-                        return;
-                    } else {
-                        if (!$remote = @fopen("ssh2.sftp://{$stream}/{$remoteDir}/{$file}", 'r')) {
-                            die("Unable to open remote file: $file\n");
-                        }
-
-                        if (!$local = @fopen($localDir . '/' . $file, 'w')) {
-                            fclose($remote);
-                            die("Unable to create local file: $file\n");
-                        }
-                        $read = 0;
-                        $filesize = filesize("ssh2.sftp://{$stream}/{$remoteDir}/{$file}");
-                        while ($read < $filesize && ($buffer = fread($remote, $filesize - $read))) {
-                            $read += strlen($buffer);
-                            if (fwrite($local, $buffer) === FALSE) {
-                                echo "Unable to write to local file: $file\n";
-                                break;
-                            }
-                        }
-                        if (file_exists($remote_file_path) && file_exists($local_file_path)) {
-                            if (filesize($remote_file_path) == filesize($local_file_path)
-                                && md5_file($remote_file_path) == md5_file($local_file_path)) {
-                                echo "Файл успешно загружен";
-                                echo "<br/>";
-                                if ($file == "specialities.xls" || $file == "specializations.xls") {
-//                                   $res = app('App\Http\Controllers\ParserController')->parseFromXls();
-//                                   echo $res;
-                                } elseif ($file == "disciplines.xls") {
-                                    $res = app('App\Http\Controllers\ParserController')->parseFromXlsSub();
-                                    echo $res;
-                                } elseif ($file == "faculties.xls") {
-                                    $res = app('App\Http\Controllers\ParserController')->parseFromXlsFaculties();
-                                    echo $res;
-                                } elseif ($file == "admission_bases.xls") {
-                                    $res = app('App\Http\Controllers\ParserController')->parseFromXlsAdmission();
-                                    echo $res;
-                                } elseif ($temp == "stat_bach_catalogs") {
-                                    $res = app('App\Http\Controllers\ParserJsonController')->parseCatalogs();
-                                    echo $res;
-                                } elseif ($temp == "stat_bach.json") {
-                                    $res = app('App\Http\Controllers\ParserJsonController')->parseFromJson();
-                                    echo $res;
-                                } elseif ($temp == "stat_master.json") {
-                                    $res = app('App\Http\Controllers\ParserJsonController')->parseFromJsonMaster();
-                                    echo $res;
-                                } elseif ($temp == "stat_asp.json") {
-                                    $res = app('App\Http\Controllers\ParserJsonController')->parseFromJsonAsp();
-                                    echo $res;
-                                } elseif ($temp == "stat_spo.json") {
-                                    $res = app('App\Http\Controllers\ParserJsonController')->parseFromJsonSpo();
-                                    echo $res;
-                                } elseif ($temp == "plans_sar_bach.json") {//plans_rim_bach
-//                                   $res = app('App\Http\Controllers\ParserJsonController')->parsePlansBach();
-//                                   echo $res;
-                                } elseif ($temp == "plans_sar_master.json") {//plans_rim_master
-//                                   $res = app('App\Http\Controllers\ParserJsonController')->parsePlansMaster();
-//                                   echo $res;
-                                } elseif ($temp == "plans_sar_asp.json") {
-//                                   $res = app('App\Http\Controllers\ParserJsonController')->parsePlansAspMain();
-//                                   echo $res;
-                                } elseif ($temp == "plans_sar_spo.json") {//plans_rim_spo //plans_kov_spo
-//                                   $res = app('App\Http\Controllers\ParserJsonController')->parsePlansSpo();
-//                                   echo $res;
-                                } elseif ($temp == "past_contests.json") {//plans_rim_spo //plans_kov_spo
-//                                   $res = app('App\Http\Controllers\ParserJsonController')->parsePastContests();
-//                                   echo $res;
-                                }
-                            }
-                        }
-                        fclose($local);
-                        fclose($remote);
-                    }
-                } elseif (file_exists($remote_file_path) && !file_exists($local_file_path)) {
-                    if (!$remote = @fopen("ssh2.sftp://{$stream}/{$remoteDir}/{$file}", 'r')) {
-                        die("Unable to open remote file: $file\n");
-                    }
-
-                    if (!$local = @fopen($localDir . '/' . $file, 'w')) {
-                        fclose($remote);
-                        die("Unable to create local file: $file\n");
-                    }
-                    $read = 0;
-                    $filesize = filesize("ssh2.sftp://{$stream}/{$remoteDir}/{$file}");
-                    while ($read < $filesize && ($buffer = fread($remote, $filesize - $read))) {
-                        $read += strlen($buffer);
-                        if (fwrite($local, $buffer) === FALSE) {
-                            echo "Unable to write to local file: $file\n";
-                            break;
-                        }
-                    }
-                    if (file_exists($remote_file_path) && file_exists($local_file_path)) {
-                        if (filesize($remote_file_path) == filesize($local_file_path)
-                            && md5_file($remote_file_path) == md5_file($local_file_path)) {
-                            echo "Файл успешно загружен";
-                        }
-                    }
-                    fclose($local);
-                    fclose($remote);
-                }
-            } else {
-                echo "Файл " . $file_name . " отсутвует на удаленном сервере";
-//            return;
-            }
-        }
+        return view('pages.files');
     }
+
 
 }
 
--- routes/web.php
+++ routes/web.php
@@ -59,19 +59,19 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/parse-admission', 'ParserController@parseAdmissionBasesLocal')->name('parse.parseAdm');
 
     //формы обучения, категории, уровни подготовки
-    Route::get('/parse-catalogs', 'ParserJsonController@parseCatalogs')->name('parse.parseCatalogs');
+    Route::get('/parse-catalogs', 'ParserJsonController@parseCatalogsLocal')->name('parse.parseCatalogs');
     //планы, цены, места
-    Route::get('/parse-plans-bach', 'ParserJsonController@parsePlansBach')->name('parse.parsePlansBach');
-    Route::get('/parse-plans-master', 'ParserJsonController@parsePlansMaster')->name('parse.parsePlansMaster');
-    Route::get('/parse-plans-asp', 'ParserJsonController@parsePlansAspMain')->name('parse.parsePlansAspMain');
-    Route::get('/parse-plans-spo', 'ParserJsonController@parsePlansSpo')->name('parse.parsePlansSpo');
+    Route::get('/parse-plans-bach', 'ParserJsonController@parsePlansBachLocal')->name('parse.parsePlansBach');
+    Route::get('/parse-plans-master', 'ParserJsonController@parsePlansMasterLocal')->name('parse.parsePlansMaster');
+    Route::get('/parse-plans-asp', 'ParserJsonController@parsePlansAspMainLocal')->name('parse.parsePlansAspMain');
+    Route::get('/parse-plans-spo', 'ParserJsonController@parsePlansSpoLocal')->name('parse.parsePlansSpo');
     //статистика приема
-    Route::get('/parse-students', 'ParserJsonController@parseFromJson')->name('parse.parseFromJson');
-    Route::get('/parse-students-master', 'ParserJsonController@parseFromJsonMaster')->name('parse.parseFromJsonMaster');
-    Route::get('/parse-students-asp', 'ParserJsonController@parseFromJsonAsp')->name('parse.parseFromJsonAsp');
-    Route::get('/parse-students-spo', 'ParserJsonController@parseFromJsonSpo')->name('parse.parseFromJsonSpo');
+    Route::get('/parse-students', 'ParserJsonController@parseStatBachAllLocal')->name('parse.parseFromJson');
+    Route::get('/parse-students-master', 'ParserJsonController@parseStatMasterAllLocal')->name('parse.parseFromJsonMaster');
+    Route::get('/parse-students-asp', 'ParserJsonController@parseStatAspAllLocal')->name('parse.parseFromJsonAsp');
+    Route::get('/parse-students-spo', 'ParserJsonController@parseStatSpoAllLocal')->name('parse.parseFromJsonSpo');
 
-    Route::get('/parse-contests', 'ParserJsonController@parsePastContests')->name('parse.parsePastContests');
+    Route::get('/parse-contests', 'ParserJsonController@parsePastContestsLocal')->name('parse.parsePastContests');
 
 
 
