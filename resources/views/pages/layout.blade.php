cb1a978707a0ad72496735fbb8fe8fc7afbb6b78
Eugene <eugen.art@mail.ru>
2021-03-26 11:40:13 +0300

    from site 2603

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -9,11 +9,11 @@
     <meta property="og:image" content="https://abiturs.mrsu.ru/storage/preview/20200128_163227_image.jpeg"/>
     <meta property="og:type" content="profile"/>
     <meta property="og:url" content="https://abiturs.mrsu.ru/"/>
-
+	<meta name="theme-color" content="#2366a5">
     <meta name="keywords"
           content="Приемная кампания, МГУ Огарева, МГУ им. Н.П. Огарева, приемная комиссия, приемная комиссия {{$year}}"/>
     <meta name="subject" content="Приемная кампания МГУ им. Н.П. Огарева {{$year}}">
-    <meta name="author" content="Арташкин Евгений, Элина Кирдяшкина">
+    <meta name="author" content="Арташкин Евгений, Кирдяшкина Элина">
     <meta name="robots" content="index,follow"/>
     <meta name="description" content="Приемная кампания МГУ им. Н.П. Огарева {{$year}}"/>
     <meta name="url" content="https://abiturs.mrsu.ru/">
@@ -271,21 +271,129 @@
 
 @section('page')
 @show
-
+ @role('admin')
+<footer>
+	<div class="container">
+		<div class="row underline-row">
+			<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4">
+				<p class="bold bottom main__corpus">Главный корпус</p>
+				<div class="position">
+					<table border="0" class="footer-table">
+						<tr>
+							<td class=""><div class="pos"><img src="{{asset('storage/images/elements/2I.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="#">Большевистская ул., 68/1</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/1E.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="mailto:dep-general@adm.mrsu.ru">dep-general@adm.mrsu.ru</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/9H.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="tel:+78342243732">+7 (8342) 24-37-32</a></div></td>
+						</tr>
+						<!-- <tr> -->
+							<!-- <td></td> -->
+							<!-- <td><p class="top-margin"><a href="" class="underline">Показать на карте</a></p></td> -->
+						<!-- </tr> -->
+					</table>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4">
+				<p class="bold bottom">Ректорат</p>
+					<div class="position">
+					<table border="0" class="footer-table">
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" class="glob" src="{{asset('storage/images/elements/6I.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="" class="">Виртуальная приёмная</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/1E.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="mailto:rector@adm.mrsu.ru">rector@adm.mrsu.ru</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/9H.svg')}}"></div></td>
+							<td><div class="pos-text"><a class="dop-text" href="tel:+78342244888">+7 (8342) 24-48-88</a></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><div class="pos-text"><span class="color__footer dop-text">+7 (8342)</span><span class="number__padding"><a class="dop-text" href="tel:+78342244757"> 24-47-57</a></span></div></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><div class="pos-text"><span class="color__footer dop-text">+7 (8342)</span><span class="number__padding"><a class="dop-text" href="tel:+78342233934"> 23-39-34</a></span></div></td>
+						</tr>
+						<!-- <tr> -->
+							<!-- <td></td> -->
+							<!-- <td><p class="top-margin"><a href="" class="underline">Показать на карте</a></p></td> -->
+						<!-- </tr> -->
+					</table>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4">
+				<p class="bold bottom">Приёмная комиссия</p>
+				<div class="position">
+					<table border="0" class="footer-table">
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/2I.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="#">Полежаева ул., 44</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/1E.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="mailto:entrance-exam@adm.mrsu.ru">entrance-exam@adm.mrsu.ru</a></div></td>
+						</tr>
+						<tr>
+							<td class=""><div class="pos"><img class="image_footer" src="{{asset('storage/images/elements/9H.svg')}}"></div></td>
+							<td><div class="pos-text"><a href="tel:+78342244835">+7 (800) 222-13-17</a></div></td>
+						</tr>
+						<!-- <tr> -->
+							<!-- <td></td> -->
+							<!-- <td><p class="top-margin"><a href="" class="underline">Показать на карте</a></p></td> -->
+						<!-- </tr> -->
+					</table>
+				</div>
+			</div>
+			<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 mb-4">
+				<p class="bold bottom">Информация</p>
+				<div class="position">
+					<p class="paa"><a href="" class="underline">Сведения об образовательной организации</a></p>
+					<p><a href="" class="underline">Противодействие коррупции</a></p>
+					<p><a href="" class="underline">Реквизиты университета</a></p>
+					<p><a href="" class="underline">Телефонный справочник</a></p>
+					<p><a href="" class="underline">Оплата услуг</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="row foot-bottom-block">
+			<div class="col-lg-6 col-md-6 col-sm-6 mb-4 col_one">
+				<p class="copyright">© 1998–2021 МГУ им. Н.П. Огарёва<br>
+				При использовании текстовой информации, фото- и видеоматериалов ссылка на сайт обязательна</p>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 mb-4 social col_two">
+				<a href=""><img src="{{asset('storage/images/elements/VK.svg')}}" alt=""></a>
+				<a href=""><img src="{{asset('storage/images/elements/Facebook.svg')}}" alt=""></a>
+				<a href=""><img src="{{asset('storage/images/elements/Twitter.svg')}}" alt=""></a>
+				<a href=""><img src="{{asset('storage/images/elements/Instagram.svg')}}" alt=""></a>
+				<a href=""><img src="{{asset('storage/images/elements/Youtube.svg')}}" alt=""></a>
+			</div>
+		</div>
+	</div>
+</footer>
+@endrole
 </body>
 <script src="{{asset('js/version.js')}}"></script>
-<script
-    src="https://code.jquery.com/jquery-3.4.1.min.js"
-    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-    crossorigin="anonymous"></script>
+<script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
 
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"--}}
 {{--        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"--}}
 {{--        crossorigin="anonymous"></script>--}}
+
 <script src="{{asset('js/popper.min.js')}}"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+
+{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"--}}
+{{--        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"--}}
+{{--        crossorigin="anonymous"></script>--}}
+
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 
 <script src="{{asset('js/bootstrap-select.js')}}"></script>
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.9/js/i18n/defaults-ru_RU.min.js"></script>--}}
