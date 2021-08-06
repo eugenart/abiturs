14a227d88059e8fabb127ad4a920cad29662f8b8
ElionBlack <elionorablack@gmail.com>
2021-08-06 16:52:01 +0300

    stage

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -622,9 +622,9 @@ class StatisticController extends Controller
                                                 $temp_stage = $temp->first();
 
                                                 $stage = $temp_stage->stage;
-                                                if ($stage[0] == '(') {
+                                                /*if ($stage[0] == '(') {
                                                     $stage = substr($stage, 1, -1);
-                                                }
+                                                }*/
                                                 $admissionBasis->stage = $stage;
 
                                                 $stage_title = $temp_stage->stage_title;
